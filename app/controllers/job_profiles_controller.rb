300b4089124dccbf9822b1f61743d8b8987fac8c
Luis Valenzuela <li.valenzuelaa@gmail.com>
2015-02-05 15:36:10 -0300

    algo de job_profiles... lo tengo que terminar mas tarde

--- app/controllers/job_profiles_controller.rb
+++ app/controllers/job_profiles_controller.rb
@@ -1,6 +1,30 @@
 class JobProfilesController < ApplicationController
-	before_filter :check_authentication
+	before_filter :set_current_user
 
 	def index
+		@job_profile = JobProfile.all()
+	end
+
+	def new
+		@job_profile = JobProfile.new()
+	end
+
+	def create
+		@job_profile = JobProfile.create(job_profile_params)
+		if @job_profile.valid?
+			redirect_to :action => :index
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def job_profile_params
+		params.require(:job_profile).permit(:name, :description)
+	end
+
+	def set_current_user
+		@current_user = current_user
 	end
 end
